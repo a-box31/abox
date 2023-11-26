4cdcaf159c95ecc3b9bdebce3a22f5cdc89a377f
a-box31 <thomaa2031@gmail.com>
2023-11-26 11:00:02 -0500

    react portfolio update

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.scss'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import Home from './components/Home'
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout></Layout>}></Route>
+        <Route path="/" element={<Layout/>} >
+          <Route index element={<Home/>}></Route>
+        </Route>
       </Routes>
     </>
   )
--- react-portfolio/src/components/Layout/index.jsx
+++ react-portfolio/src/components/Layout/index.jsx
@@ -5,20 +5,25 @@ import { Outlet } from 'react-router-dom'
 const Layout = () => {
   return (
     <div className="App">
+
       <SideBar />
+
       <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
+        <span className="tags top-tags">
+          <span className="top-tag-html">&lt;html&gt;</span>
+          <br />
+          &lt;body&gt;
+        </span>
 
         <Outlet />
 
         <span className="tags bottom-tags">
-            &lt;body&gt;
-            <br />
-            <span className="bottom-tag-html">&lt;/html&gt;</span>
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
         </span>
-
-
       </div>
+      
     </div>
   )
 }
