732deda32187d4ae0022f1a22526ed249cd9d87b
a-box31 <thomaa2031@gmail.com>
2024-06-11 00:10:54 -0400

    Creating First React Components

---
+++ react-portfolio/src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+function Header() {
+
+    return (
+        <header class="navigation">
+            <div className="logo">
+
+            </div>
+            <div className="navbar">
+                <ul>
+                    <li>Link - 1</li>
+                    <li>Link - 2</li>
+                    <li>Link - 3</li>
+                </ul>
+            </div>
+        </header>
+    )
+}
