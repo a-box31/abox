513ee6ebaf16baaadd8fbe3cb4e5627b436b4ff3
a-box31 <thomaa2031@gmail.com>
2024-06-14 08:49:32 -0400

    Resume Fixes

--- react-portfolio/src/components/Resume/index.jsx
+++ react-portfolio/src/components/Resume/index.jsx
@@ -2,7 +2,6 @@ import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState, useEffect } from 'react'
-import resume from '../../assets/Resume.pdf'
 
 const Resume = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -36,7 +35,7 @@ const Resume = () => {
             // src="https://docs.google.com/document/d/e/2PACX-1vTVPPGlooq-xpmcnC7meFs3UDXhSSXVoenpk_kaddPDuhJzhhKqcMQb_xqO9UfSIRsUBVlcub3cAwQw/pub?embedded=true"
           ></object> */}
           <iframe
-            src="https://drive.google.com/file/d/1lk__676qubFdK7mKUme1N4ypljSbS1Gg/preview"
+            src="https://drive.google.com/file/d/1Us-zJTWO-UDT6zCyiTnT7Vun4TQ6zngW/preview"
             className="resume"
             allow="autoplay"
           ></iframe>
