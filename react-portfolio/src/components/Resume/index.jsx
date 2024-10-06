176bf27843ce69865c534bb70d5afacd908691b1
a-box31 <thomaa2031@gmail.com>
2024-10-06 12:43:48 -0400

    Updating Resume

--- react-portfolio/src/components/Resume/index.jsx
+++ react-portfolio/src/components/Resume/index.jsx
@@ -35,7 +35,7 @@ const Resume = () => {
             // src="https://docs.google.com/document/d/e/2PACX-1vTVPPGlooq-xpmcnC7meFs3UDXhSSXVoenpk_kaddPDuhJzhhKqcMQb_xqO9UfSIRsUBVlcub3cAwQw/pub?embedded=true"
           ></object> */}
           <iframe
-            src="https://drive.google.com/file/d/1Us-zJTWO-UDT6zCyiTnT7Vun4TQ6zngW/preview"
+            src="https://drive.google.com/file/d/1byIFQFeuOOi1EhECDXodaphEEd9xUrcZ/preview"
             className="resume"
             allow="autoplay"
           ></iframe>
